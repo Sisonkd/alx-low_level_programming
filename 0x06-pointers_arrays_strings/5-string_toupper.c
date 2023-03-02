8d960b3c4a8f3e45c95ec962d02318d4df2f57d2
Sisonke <sisonkesamkele@gmail.com>
2023-03-01 18:33:10 -0800

    Always look up

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes all lowercase letters of a string to uppercase.
+ * @x: To the  string.
+ *
+ * Return: To the uppercase string.
+ */
+
+char *string_toupper(char *x)
+{
+	int length;
+
+	length = 0;
+
+	while (x[length] != '\0')
+	{
+		if (x[length] >= 97 && x[length] <= 122)
+		{
+			x[length] = x[length] - 32;
+		}
+		length++;
+	}
+	return (x);
+}
